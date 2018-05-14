fa4b1b7cb57ee4df3434ceae8b03d782221e62ce
Marina_Chekulina002796 <tchekulina@ya.ru>
2018-05-14 22:39:05 +0300

    Update design in %

--- gibdd_application/settings.py
+++ gibdd_application/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3t+e+h0^+qdh!zorxuoiq(!dql0b130tt$8#7a%o0)e_l58^#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
